e19e7c5b77c08abe48c02c8ed5084272097d863f
Zekarias Fikru <zekariasfekeru@gmail.com>
2024-10-10 15:56:25 +0300

    3-main.js

--- 3-main.js
+++ 3-main.js
@@ -1,4 +1,15 @@
 const para = document.querySelector('p');
 
 //console.log(para.innerText);
-para.innerText += ' JS IS AWESOME'
+//para.innerText += ' JS IS AWESOME'
+
+/*const paras = document.querySelectorAll('p');
+paras.forEach(para => {
+    console.log(para.innerText);
+    para.innerText += 'new text'
+})*/
+
+const content = document.querySelector('.content');
+//console.log(content.innerHTML);
+content.innerHTML += '<h2>THIS IS NEW</h2>'
+ 
